e4a154ab8f271c45c575f9e5f4a4c30cdd556a04
Tymoteusz Budzinski <tymoteusz.budziski123@wp.pl>
2024-02-09 20:42:13 +0100

    loop_in_jinja

--- app.py
+++ app.py
@@ -2,6 +2,31 @@ from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
+class Currency:
+
+    def __init__(self, code, name, flag):
+        self.code = code
+        self.name = name
+        self.flag = flag
+
+    def __repr__(self):
+        return '<Currency {}>'.format(self.code)
+
+class CantorOffer:
+    def __init__(self):
+        self.currencies = []
+
+    def load_offer(self):
+        self.currencies.append(Currency('USD', 'Dollar', 'dollar.jpg'))
+        self.currencies.append(Currency('EUR', 'Euro', 'euro.jpg'))
+        self.currencies.append(Currency('YEN', 'YEN', 'jen.jpg'))
+
+    def get_by_code(self, code):
+        for currency in self.currencies:
+            if currency.code == code:
+                return currency
+        return Currency('unknow', 'unknow', 'pirat.jpg')
+
 @app.route('/')
 def index():
 
@@ -9,8 +34,12 @@ def index():
 
 @app.route('/exchange', methods=['GET', 'POST'])
 def exchange():
+
+    offer = CantorOffer()
+    offer.load_offer()
+
     if request.method == 'GET':
-        return render_template('exchange.html')
+        return render_template('exchange.html', offer=offer)
     else:
         currency = 'EUR'
         if 'currency' in request.form:
@@ -20,5 +49,6 @@ def exchange():
         if 'ammount' is request.form:
             ammount = request.form['ammount']
 
-        return render_template('exchange_result.html', currency=currency, ammount=ammount)
+        return render_template('exchange_result.html', currency=currency, ammount=ammount,
+                               currency_info=offer.get_by_code(currency))
 
