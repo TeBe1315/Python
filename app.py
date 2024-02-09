d1ff303ec6708be9688a108adb5b70124fb1a919
Tymoteusz Budzinski <tymoteusz.budziski123@wp.pl>
2024-02-09 19:32:58 +0100

    create_templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,19 @@ app = Flask(__name__)
 def index():
 
     return 'This is new project'
+
+@app.route('/exchange', methods=['GET', 'POST'])
+def exchange():
+    if request.method == 'GET':
+        return render_template('exchange.html')
+    else:
+        currency = 'EUR'
+        if 'currency' in request.form:
+            currency = request.form['currency']
+
+        ammount = 100
+        if 'ammount' is request.form:
+            ammount = request.form['ammount']
+
+        return render_template('exchange_result.html', currency=currency, ammount=ammount)
+
