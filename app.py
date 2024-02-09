3600b06a5000cbb9fabbcb747d250c12cdbc286b
Tymoteusz Budzinski <tymoteusz.budziski123@wp.pl>
2024-02-09 21:39:09 +0100

    flash_in_flask

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SNC'
 
 class Currency:
 
@@ -15,17 +16,19 @@ class Currency:
 class CantorOffer:
     def __init__(self):
         self.currencies = []
+        self.denied_codes = []
 
     def load_offer(self):
-        self.currencies.append(Currency('USD', 'Dollar', 'dollar.jpg'))
-        self.currencies.append(Currency('EUR', 'Euro', 'euro.jpg'))
-        self.currencies.append(Currency('YEN', 'YEN', 'jen.jpg'))
+        self.currencies.append(Currency('USD', 'Dollar', 'flag_dollar.jpg'))
+        self.currencies.append(Currency('EUR', 'Euro', 'flag_euro.jpg'))
+        self.currencies.append(Currency('YEN', 'YEN', 'flag_jen.jpg'))
+        self.denied_codes.append('USD')
 
     def get_by_code(self, code):
         for currency in self.currencies:
             if currency.code == code:
                 return currency
-        return Currency('unknow', 'unknow', 'pirat.jpg')
+        return Currency('unknow', 'unknow', 'flag_pirat.jpg')
 
 @app.route('/')
 def index():
@@ -45,6 +48,12 @@ def exchange():
         if 'currency' in request.form:
             currency = request.form['currency']
 
+        if currency in offer.denied_codes:
+            flash('This currency {} cannot be accepted'.format(currency))
+        elif offer.get_by_code(currency) == 'unknow':
+            flash('This selected currency is unknow and be cannot be accepted')
+        else:
+            flash('Request to exchenge {} was accepted'. format(currency))
         ammount = 100
         if 'ammount' is request.form:
             ammount = request.form['ammount']
